9850ccea12c33cd23ad9f2e56d1ec6b5e44c97ce
lixiaodong <lixiaodong@coolfishgame.com>
2017-10-27 19:09:37 +0800

    config refresh

---
+++ config-client/src/main/java/com/coolfish/configclient/RefreshConfig.java
@@ -0,0 +1,13 @@
+package com.coolfish.configclient;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Service;
+
+/**
+ * author : Forrest
+ * date : 2017/10/27.
+ */
+@Service
+@RefreshScope
+public class RefreshConfig {
+}
--- config-client/src/main/java/com/coolfish/configclient/Test.java
+++ config-client/src/main/java/com/coolfish/configclient/Test.java
@@ -1,22 +1,30 @@
 package com.coolfish.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * author : Forrest
  * date : 2017/10/27.
  */
-@Component
+@RestController
 @EnableScheduling
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class Test {
     @Value("${testiflocalfirst}")
     private String gitRemoteContent;
 
     @Scheduled(initialDelayString = "1000", fixedRateString = "1000")
-    private void sysout() {
+    public void sysout() {
         System.out.println("output :" + gitRemoteContent);
+    }
+
+    @RequestMapping("/hello")
+    public String fresh() {
+        return "received refresh request";
     }
 }
