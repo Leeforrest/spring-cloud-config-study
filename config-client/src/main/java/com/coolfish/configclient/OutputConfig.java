d77dd7415c7f08f1e7b8a0862ad56858f8421a40
lixiaodong <lixiaodong@coolfishgame.com>
2017-10-30 16:22:48 +0800

    change test

--- config-client/src/main/java/com/coolfish/configclient/OutputConfig.java
+++ config-client/src/main/java/com/coolfish/configclient/OutputConfig.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableScheduling
 @RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
-public class Test {
+public class OutputConfig {
     @Value("${test}")
     private String gitRemoteContent;
 
